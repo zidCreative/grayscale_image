8e378a662a1fa46768ce596aa4678dd47a7651cf
zidCreative <124875627+zidCreative@users.noreply.github.com>
2023-02-10 10:56:05 +0700

    Add files via upload

---
+++ canny_image.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from PIL import Image
+
+st.sidebar.image("Resources\zidCreative_putih.png")
+
+st.title("Program Konversi gambar Grayscale")
+gamb = st.file_uploader("Upload gambar yang ingin anda konversi", type=['jpg' , 'png' ])
+
+if gamb is not None :
+    fileg = Image.open(gamb)
+    st.text("Gambar Original")
+    st.image(fileg)
+    
+    size = fileg.size
+    st.text(size)
+    greyscale = fileg.convert("L")
+        
+    tombol = st.button("Konversi Gambar")
+    if tombol :
+        st.text("Gambar Grayscale")
+        st.image(greyscale)
+        st.success("Gambar berhasil dikonversi, klik kanan pada gambar untuk untuk menyimpan")
+        
+            
+
